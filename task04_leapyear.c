302db2269c0ece96bd1cbb63d982c4fe1f099c2c
Surhandeedar <s.urhandeedar.9@gmail.com>
2026-09-08 13:14:05 +0500

    In lab tasks

---
+++ task04_leapyear.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	int year;
+	printf("Enter a year: ");
+	scanf("%d", &year);
+	if(year%4==0 && year%100!=0) {
+		printf("It is a Leap year");
+	}
+	else {
+		printf("It is not a Leap year");
+	}
+	return 0;
+	}
